d3f3609c16575065d9b8ba5274efe79eb6f98e17
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-06-30 15:54:22 -0400

    Adjust visibility

--- app/src/main/java/com/example/android/montrealtourguide/AttractionAdapter.java
+++ app/src/main/java/com/example/android/montrealtourguide/AttractionAdapter.java
@@ -31,26 +31,41 @@ public class AttractionAdapter extends ArrayAdapter<Attraction> {
         Attraction currentAttraction = getItem(position);
 
         ImageView iconImageView = (ImageView) listItemView.findViewById(R.id.image);
-        if(currentAttraction.hasImage()) iconImageView.setImageResource(currentAttraction.getImageResourceID());
+        if(currentAttraction.hasImage()){
+            iconImageView.setImageResource(currentAttraction.getImageResourceID());
+            iconImageView.setVisibility(View.VISIBLE);
+        }
         else iconImageView.setVisibility(View.GONE);
 
         TextView nameTextView = (TextView) listItemView.findViewById(R.id.name);
         nameTextView.setText(currentAttraction.getName());
 
         TextView streetTextView = (TextView) listItemView.findViewById(R.id.street);
-        if (currentAttraction.hasStreet()) streetTextView.setText(currentAttraction.getStreet());
+        if (currentAttraction.hasStreet()){
+            streetTextView.setText(currentAttraction.getStreet());
+            streetTextView.setVisibility(View.VISIBLE);
+        }
         else streetTextView.setVisibility(View.GONE);
 
         TextView cityTextView = (TextView) listItemView.findViewById(R.id.city);
-        if (currentAttraction.hasCity()) cityTextView.setText(currentAttraction.getCity());
+        if (currentAttraction.hasCity()) {
+            cityTextView.setText(currentAttraction.getCity());
+            cityTextView.setVisibility(View.VISIBLE);
+        }
         else cityTextView.setVisibility(View.GONE);
 
         TextView postalCodeTextView = (TextView) listItemView.findViewById(R.id.postalCode);
-        if (currentAttraction.hasPostalCode()) postalCodeTextView.setText(currentAttraction.getPostalCode());
+        if (currentAttraction.hasPostalCode()){
+            postalCodeTextView.setText(currentAttraction.getPostalCode());
+            postalCodeTextView.setVisibility(View.VISIBLE);
+        }
         else postalCodeTextView.setVisibility(View.GONE);
 
         TextView datesTextView = (TextView) listItemView.findViewById(R.id.dates);
-        if (currentAttraction.hasDates()) datesTextView.setText(currentAttraction.getDates());
+        if (currentAttraction.hasDates()) {
+            datesTextView.setText(currentAttraction.getDates());
+            datesTextView.setVisibility(View.VISIBLE);
+        }
         else datesTextView.setVisibility(View.GONE);
 
         return listItemView;
