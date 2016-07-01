e57ca14d955ea6d8a554fefbec2557fae0492321
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-06-30 22:50:15 -0400

    Add up button

--- app/src/main/java/com/example/android/montrealtourguide/MuseumActivity.java
+++ app/src/main/java/com/example/android/montrealtourguide/MuseumActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.montrealtourguide;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
